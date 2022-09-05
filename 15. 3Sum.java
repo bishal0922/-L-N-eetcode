5523fe2ccf051c0c3d00a38aca573e4966e8bb05
Bishal Giri <bishal0922@gmail.com>
2022-09-05 13:31:03 -0500

    Update 15. 3Sum.java

--- 15. 3Sum.java
+++ 15. 3Sum.java
@@ -1,11 +1,10 @@
-//Neetcode -> Two Pointers 
-
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         //This problem can be though of as the twoSum problem with a extra step
         //goal is to find triplets that sum up to 0
         //so for every number we find its complement (0-num[i]) then
         //we can apply the different two sum methods (hashmap || two pointers) to find the remaining two that sum to (0-num[i])
+        //--the hashmap method does return duplicate triplets
         
         /*
         This problem can be though of as the twoSum problem with a extra step.
@@ -28,8 +27,7 @@ class Solution {
                
             //we declare our target along with the two pointers
             int target = 0-nums[i];
-            //i+1 as i would be part of target [i, __, __]
-            int left= i+1;
+            int left= i+1; //i+1 as i would be part of target [i, __, __]
             int right = nums.length-1;
             
             while (left<right){
@@ -48,9 +46,17 @@ class Solution {
                 while (nums[right] == nums[right-1]){
                     right--;
                 }
+                /*It can be confusing to why we move the pointers again as in the while loop and the code segment below this comment
+                Imagine a section of code [2,2,2,4,5]
+                The while loop above guarantees that the left pointer with be at the 3rd "2" and since 2 was already added as a triplet from 1st "2",
+                We move our left pointer to "4" and then further begin our search
+                In short, the while loop takes us to the last occurence and the code below makes sure pointer is infront of it. 
+                */
                 left++;
                 right--;
                 } else if (nums[left] + nums[right] > target){
+                  //In the sorted array if the number is larger than target, It's the right pointer that needs to be moved as
+                  //nums[left] always < nums[right]  
                     right--;
                 } else{
                     left++;
@@ -58,6 +64,8 @@ class Solution {
             }
         }
         }
+        
+        //return distinct triplets
         return list;
     }
 }
